0a30bb51d7d3d89edef193aa60d58dd8c50f9657
Ricardo Tique <Ricardo Tique@Solar_I>
2017-12-01 23:03:26 -0500

    -Se realiza correcion al metodo de fitness. -Se agrega el ordenamiento de los beneficios.

--- Mochila/Mochila/src/knapsackProblem/genetic/FitnessFunction.java
+++ Mochila/Mochila/src/knapsackProblem/genetic/FitnessFunction.java
@@ -8,62 +8,109 @@
 #
 # GA_TravellingThiefProblem is free software: you can redistribute it and/or modify it under the terms of the
 # GNU General Public License as published by the Free Software Foundation, version 2.
-*/
+ */
 package knapsackProblem.genetic;
+
 /**
  *
  * @author Ricardo Tique & Carlos Andres Sierra
  */
 public class FitnessFunction {
 
-    private int volumenMochila;
-    private int[] pesosMohcila;
+    private int knapsackCapacity;
+    private int[] productsWeight;
     private int[] benefit;
+    public int[][] orderBenefit;
 
-    public FitnessFunction(int volumenMochila, int[] pesosMohcila, int[] benefit) {
-        this.volumenMochila = volumenMochila;
-        this.pesosMohcila = pesosMohcila;
+    public FitnessFunction(int knapsackCapacity, int[] productsWeight, int[] benefit) {
+        this.knapsackCapacity = knapsackCapacity;
+        this.productsWeight = productsWeight;
         this.benefit = benefit;
+        this.orderBenefit();
     }
 
-    public int calculate(int[] genotipo) {
-        int fitnes = 0;
-        for (int i = 0; i < genotipo.length; i++) {
-            if (genotipo[i] == 1) {
-                fitnes += getPesosMohcila()[i];
+    public int calculateFitness(int[] genotype) {
+        int fitness = 0;
+        if (calculateWeight(genotype) <= knapsackCapacity) {
+            for (int i = 0; i < genotype.length; i++) {
+                if (genotype[i] == 1) {
+                    fitness += this.benefit[i];
+                }
+            }
+        }else{
+            fitness = -1;
+        }
+        return fitness;
+    }
+
+    private int calculateWeight(int[] genotype) {
+        int weight = 0;
+        for (int i = 0; i < genotype.length; i++) {
+            if (genotype[i] == 1) {
+                weight += this.productsWeight[i];
             }
         }
-        if (fitnes > getVolumenMochila()) {
-            return 0;
-        } else {
-            return fitnes;
-        }
+        return weight;
     }
 
-    public int calculateBenefit(int[] genotipo) {
-        int costo = 0;
-        for (int i = 0; i < genotipo.length; i++) {
-            if (genotipo[i] == 1) {
-                costo += getBenefit()[i];
+    private void orderBenefit() {
+        this.orderBenefit = new int[this.benefit.length][2];
+        for (int i = 0; i < this.benefit.length; i++) {
+            this.orderBenefit[i][0] = i;
+            this.orderBenefit[i][1] = this.benefit[i];
+        }
+        orderMethot(this.orderBenefit, 0, this.benefit.length - 1);
+    }
+
+    private void orderMethot(int[][] vector, int izquierda, int derecha) {
+        int indicePivote = vector[izquierda][0];
+        int pivote = vector[izquierda][1];
+        int i = izquierda;
+        int j = derecha;
+        int auxIndice;
+        int auxPeso;
+        while (i < j) {
+            while (vector[i][1] <= pivote && i < j) {
+                i++;
+            }
+            while (vector[j][1] > pivote) {
+                j--;
+            }
+            if (i < j) {
+                auxIndice = vector[i][0];
+                auxPeso = vector[i][1];
+                vector[i][0] = vector[j][0];
+                vector[i][1] = vector[j][1];
+                vector[j][0] = auxIndice;
+                vector[j][1] = auxPeso;
             }
         }
-        return costo;
+        vector[izquierda][0] = vector[j][0];
+        vector[izquierda][1] = vector[j][1];
+        vector[j][0] = indicePivote;
+        vector[j][1] = pivote;
+        if (izquierda < j - 1) {
+            orderMethot(vector, izquierda, j - 1);
+        }
+        if (j + 1 < derecha) {
+            orderMethot(vector, j + 1, derecha);
+        }
     }
 
-    public int getVolumenMochila() {
-        return volumenMochila;
+    public int getKnapsackCapacity() {
+        return knapsackCapacity;
     }
 
-    public void setVolumenMochila(int volumenMochila) {
-        this.volumenMochila = volumenMochila;
+    public void setKnapsackCapacity(int knapsackCapacity) {
+        this.knapsackCapacity = knapsackCapacity;
     }
 
-    public int[] getPesosMohcila() {
-        return pesosMohcila;
+    public int[] getProductsWeight() {
+        return productsWeight;
     }
 
-    public void setPesosMohcila(int[] pesosMohcila) {
-        this.pesosMohcila = pesosMohcila;
+    public void setProductsWeight(int[] productsWeight) {
+        this.productsWeight = productsWeight;
     }
 
     public int[] getBenefit() {
